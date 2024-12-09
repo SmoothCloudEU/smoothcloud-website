1c00951b162fe1a148c4558ba6cd541556f4b440
ezTxmMC <eztxmmc@hotmail.com>
2024-12-09 18:23:39 +0100

    started with new language system

--- public/javascript/translate.js
+++ public/javascript/translate.js
@@ -1,3 +1,5 @@
+const languageSelect = document.getElementById("language-select");
+
 async function readLanguageFile(file) {
   try {
     const response = await fetch(file);
@@ -13,15 +15,12 @@ async function readLanguageFile(file) {
 }
 
 async function changeLanguage(language) {
-  const currentLanguage = document.getElementById("current-language");
-  const pageTitle = document.getElementById("pageTitle");
-  const wartungsText = document.getElementById("wartungsText");
+  const currentLanguage = document.getElementById("current-language-icon");
   const readLanguage = await readLanguageFile(`/public/lang/${language}.json`);
   if (readLanguage) {
+    window.localStorage.setItem("language", language);
     currentLanguage.src = `/public/image/icon/${readLanguage.imageName}.png`;
-    pageTitle.textContent = readLanguage.pageTitle;
-    wartungsText.textContent = readLanguage.maintenanceText;
-  } else {
-    console.warn("Sprachdatei konnte nicht geladen werden.");
+    return;
   }
+  console.warn("Language-File can't be loaded");
 }
